d034f312f4dd9850bd2344feeae3ae342705f1d1
akm0012 <akm0012@auburn.edu>
2013-05-31 13:56:49 -0500

    Backup before I mess with manifest.  Everything is working pretty well except when the screen is rotated.

--- src/edu/auburn/akm0012/fl_roughdraft/LogIn.java
+++ src/edu/auburn/akm0012/fl_roughdraft/LogIn.java
@@ -1,7 +1,10 @@
 package edu.auburn.akm0012.fl_roughdraft;
 
+import android.annotation.SuppressLint;
 import android.app.Activity;
+import android.graphics.drawable.TransitionDrawable;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.text.InputType;
 import android.view.View;
 import android.view.WindowManager;
@@ -9,7 +12,9 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.RelativeLayout;
 import android.widget.RelativeLayout.LayoutParams;
+import android.widget.Toast;
 
+@SuppressLint("ShowToast")
 public class LogIn extends Activity {
 
 	private RelativeLayout loginLayout;
@@ -17,6 +22,10 @@ public class LogIn extends Activity {
 	private Button newUserButton, logInButton;
 
 	private boolean isNewUser;
+
+	Toast testToasts_FailedLogin_PassDontMatch, testToasts_SuccessfulLogin,
+			testToasts_FailedLogin_UsernameBlank,
+			testToasts_FailedLogin_PasswordBlank;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -34,20 +43,92 @@ public class LogIn extends Activity {
 
 		isNewUser = false;
 
+		testToasts_FailedLogin_PassDontMatch = Toast.makeText(this,
+				"LogIn Failed, Passwords Don't Match", Toast.LENGTH_SHORT);
+
+		testToasts_SuccessfulLogin = Toast.makeText(this,
+				"LogIn Successfull!!", Toast.LENGTH_SHORT);
+
+		testToasts_FailedLogin_UsernameBlank = Toast.makeText(this,
+				"LogIn Failed, Username Blank", Toast.LENGTH_SHORT);
+
+		testToasts_FailedLogin_PasswordBlank = Toast.makeText(this,
+				"LogIn Failed, Password Blank", Toast.LENGTH_SHORT);
+
 		// User clicks the "New User" button
 		newUserButton.setOnClickListener(new View.OnClickListener() {
 			public void onClick(View v) {
 
 				if (!isNewUser) {
-					// Create new Confirm Password Field
+					// Create new Confirm Password Field, changes the button to
+					// a register button.
 					addPasswordConfirmField();
 				}
 
 				else {
-					// Put the "Register" method here
-					executeLogin();
-				}
 
+					// Check to make sure the 2 password fields match
+					if (!passwordField.getText().toString()
+							.matches(confirmPasswordField.getText().toString())) {
+
+						testToasts_FailedLogin_PassDontMatch.show();
+
+						passwordField.setText("");
+						confirmPasswordField.setText("");
+
+						passwordField.setHint("Password");
+						confirmPasswordField.setHint("Confirm Password");
+
+						loginLayout
+								.setBackgroundResource(R.drawable.red_gradient);
+
+					}
+
+					// Check for blank User Name Field
+					else if (userNameField.getText().toString().matches("")) {
+
+						testToasts_FailedLogin_UsernameBlank.show();
+
+						userNameField.setText("");
+						userNameField.setHint("User Name");
+
+						loginLayout
+								.setBackgroundResource(R.drawable.red_gradient);
+
+					}
+
+					// Check if password field is blank (Only need to check this
+					// field, since we check if the confirm filed matches
+					// earlier)
+					else if (passwordField.getText().toString().matches("")) {
+
+						testToasts_FailedLogin_PasswordBlank.show();
+
+						passwordField.setText("");
+						confirmPasswordField.setText("");
+
+						passwordField.setHint("Password");
+						confirmPasswordField.setHint("Confirm Password");
+
+						loginLayout
+								.setBackgroundResource(R.drawable.red_gradient);
+
+					}
+
+					else {
+						// userNameField.setHint("Type user)
+						//
+						// testToasts_FailedLogin.show();
+
+						registerNewUser(userNameField.getText().toString(),
+								passwordField.getText().toString());
+						testToasts_SuccessfulLogin.show();
+
+						loginLayout
+								.setBackgroundResource(R.drawable.green_gradient);
+
+					}
+				}
 			}
 		});
 
@@ -65,6 +146,26 @@ public class LogIn extends Activity {
 				WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
 	}
 
+	//This may be helpful when rotating the screen
+	@Override
+	protected void onRestoreInstanceState(Bundle savedInstanceState) {
+		super.onRestoreInstanceState(savedInstanceState);
+		
+	}
+	
+	// Couldn't get this to work, maybe come back to later.
+	// private void changeBackround() {
+	//
+	// loginLayout.setBackgroundResource(R.drawable.green_red_transition);
+	//
+	// final TransitionDrawable transition = (TransitionDrawable) loginLayout
+	// .getBackground();
+	//
+	// //transition.setCrossFadeEnabled(true);
+	// transition.startTransition(2000);
+	//
+	// }
+
 	private void addPasswordConfirmField() {
 
 		// Set the Layout parameters for the new EditText
@@ -78,9 +179,8 @@ public class LogIn extends Activity {
 				| InputType.TYPE_TEXT_VARIATION_PASSWORD);
 		confirmPasswordField.setHint("Confirm Password");
 		confirmPasswordField.setEms(10);
+		confirmPasswordField.setTextAppearance(this, android.R.style.TextAppearance_Large);
 		loginLayout.addView(confirmPasswordField, layoutParams);
-
-		
 
 		loginLayout.removeView(logInButton);
 
@@ -92,14 +192,50 @@ public class LogIn extends Activity {
 
 	}
 
-	// Used as a placeholder, may not be necessary.
-	public void executeLogin() {
-		// Intent intent = new Intent(this, FragmentSwitcher.class);
-		// startActivity(intent);
+	// Returns a boolean if the login was successful.
+	private boolean registerNewUser(String newUserName, String newPassword) {
+
+		return false;
 	}
 
-	public void executeLogin(View view) {
-		// Intent intent = new Intent(this, ModuleSelectorActivity.class);
+	// Used as a placeholder, may not be necessary.
+	public void executeLogin() {
+
+		String username, password;
+
+		username = userNameField.getText().toString();
+		password = passwordField.getText().toString();
+
+		// Username field is blank
+		if (username.matches("")) {
+			// Turn background red
+			userNameField.setHint("Username missing.");
+
+			testToasts_FailedLogin_UsernameBlank.show();
+			loginLayout.setBackgroundResource(R.drawable.red_gradient);
+		}
+
+		else {
+			// Continue Login Progress
+		}
+
+		// Password field is blank
+		if (password.matches("")) {
+			// Turn background red
+			passwordField.setHint("Password missing.");
+
+			testToasts_FailedLogin_PasswordBlank.show();
+			loginLayout.setBackgroundResource(R.drawable.red_gradient);
+		}
+
+		else {
+			// Continue Login Progress
+			testToasts_SuccessfulLogin.show();
+			loginLayout.setBackgroundResource(R.drawable.green_gradient);
+
+		}
+
+		// Intent intent = new Intent(this, FragmentSwitcher.class);
 		// startActivity(intent);
 	}
 
