5ed171910b378e4d617eaa21c6bead9cf66fa25e
akm0012 <akm0012@auburn.edu>
2013-06-06 14:48:01 -0500

    After meeting, things working!

--- src/edu/auburn/akm0012/fl_roughdraft/MainModuleSelector.java
+++ src/edu/auburn/akm0012/fl_roughdraft/MainModuleSelector.java
@@ -31,6 +31,8 @@ public class MainModuleSelector extends Activity {
 		setContentView(R.layout.activity_main_module_selector);
 
 		if (getResources().getConfiguration().orientation == Configuration.ORIENTATION_PORTRAIT) {
+
+			// Create the right sliding menu
 			SlidingMenu rightMenu;
 			rightMenu = new SlidingMenu(this);
 			rightMenu.setMode(SlidingMenu.RIGHT);
@@ -42,7 +44,8 @@ public class MainModuleSelector extends Activity {
 			rightMenu.attachToActivity(this, SlidingMenu.SLIDING_CONTENT);
 			rightMenu.setBehindWidth(200);
 			rightMenu.setMenu(R.layout.right_menu_frame);
-			
+
+			// Create the left sliding menu
 			SlidingMenu leftMenu;
 			leftMenu = new SlidingMenu(this);
 			leftMenu.setMode(SlidingMenu.LEFT);
@@ -70,6 +73,37 @@ public class MainModuleSelector extends Activity {
 
 		// Else phone is in orientation mode
 		else {
+
+			// Create the left STATIC menu
+			SlidingMenu leftMenu;
+			leftMenu = new SlidingMenu(this);
+			leftMenu.setMode(SlidingMenu.LEFT);
+			// This will make the menu only open when you swipe near the edge of
+			// the screen. Other option is FULLSCREEN
+			leftMenu.setTouchModeAbove(SlidingMenu.TOUCHMODE_NONE);
+			leftMenu.setShadowWidth(5);
+			leftMenu.setFadeDegree(0.0f);
+			leftMenu.attachToActivity(this, SlidingMenu.SLIDING_CONTENT);
+			leftMenu.setBehindWidth(200);
+			leftMenu.setSlidingEnabled(false);
+			leftMenu.showMenu();
+			leftMenu.setMenu(R.layout.left_menu_frame);
+
+			// Create the right sliding menu
+			SlidingMenu rightMenu;
+			rightMenu = new SlidingMenu(this);
+			rightMenu.setMode(SlidingMenu.RIGHT);
+			// This will make the menu only open when you swipe near the edge of
+			// the screen. Other option is FULLSCREEN
+			rightMenu.setTouchModeAbove(SlidingMenu.TOUCHMODE_MARGIN);
+			rightMenu.setShadowWidth(5);
+			rightMenu.setFadeDegree(0.0f);
+			rightMenu.attachToActivity(this, SlidingMenu.SLIDING_CONTENT);
+			rightMenu.setBehindWidth(200);
+			rightMenu.setMenu(R.layout.right_menu_frame);
+
+			navBar = (LinearLayout) leftMenu.findViewById(R.id.navBar);
+
 			FragmentManager frgman = getFragmentManager();
 			FragmentTransaction frgTrans = frgman.beginTransaction();
 			frgTrans.commit();
@@ -124,28 +158,33 @@ public class MainModuleSelector extends Activity {
 
 			Fragment gravFrag = new gravity_Fragment();
 
-			fragmentTransaction.replace(R.id.moduleSelectorFragment_Landscape,
+			fragmentTransaction.remove(moduleSelector);
+
+			fragmentTransaction.add(R.id.moduleSelectorFragment_Landscape,
 					gravFrag, "GRAV_FRAG_LAND");
 
-			Fragment moduleFrag = fragmentManager
-					.findFragmentByTag("MODULE_FRAG_LAND");
+			// fragmentTransaction.replace(R.id.moduleSelectorFragment_Landscape,
+			// gravFrag, "GRAV_FRAG_LAND");
 
-			fragmentTransaction.remove(moduleFrag);
+			// Fragment moduleFrag = fragmentManager
+			// .findFragmentByTag("MODULE_FRAG_LAND");
+			//
+			// fragmentTransaction.remove(moduleFrag);
 
-			// ImageButton gravShortcut = new ImageButton(this);
-			//
-			// gravShortcut.setImageResource(R.drawable.module_icon_gravity);
-			//
-			// // Set the shortcut layout params
-			// LinearLayout.LayoutParams layoutParams = new
-			// LinearLayout.LayoutParams(
-			// LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
-			//
-			// // Make it fit in the sliding menu
-			// // gravShortcut.setScaleType(ScaleType.FIT_XY);
-			// gravShortcut.setScaleType(ScaleType.CENTER_INSIDE);
-			//
-			// navBar.addView(gravShortcut, layoutParams);
+			ImageButton gravShortcut = new ImageButton(this);
+
+			gravShortcut.setImageResource(R.drawable.module_icon_gravity);
+
+			// Set the shortcut layout params
+			LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+					LayoutParams.MATCH_PARENT, navBar.findViewById(
+							R.id.Module_Button).getHeight());
+
+			// Make it fit in the sliding menu
+			// gravShortcut.setScaleType(ScaleType.FIT_XY);
+			gravShortcut.setScaleType(ScaleType.CENTER_INSIDE);
+
+			navBar.addView(gravShortcut, layoutParams);
 		}
 
 		fragmentTransaction.addToBackStack(null);
@@ -195,28 +234,33 @@ public class MainModuleSelector extends Activity {
 
 			Fragment univ_GravFrag = new univ_Gravity_Fragment();
 
-			fragmentTransaction.replace(R.id.moduleSelectorFragment_Landscape,
+			fragmentTransaction.remove(moduleSelector);
+
+			fragmentTransaction.add(R.id.moduleSelectorFragment_Landscape,
 					univ_GravFrag, "UNIV_GRAV_FRAG_LAND");
 
-			Fragment moduleFrag = fragmentManager
-					.findFragmentByTag("MODULE_FRAG_LAND");
+			// fragmentTransaction.replace(R.id.moduleSelectorFragment_Landscape,
+			// univ_GravFrag, "UNIV_GRAV_FRAG_LAND");
+			//
+			// Fragment moduleFrag = fragmentManager
+			// .findFragmentByTag("MODULE_FRAG_LAND");
+			//
+			// fragmentTransaction.remove(moduleFrag);
 
-			fragmentTransaction.remove(moduleFrag);
+			ImageButton univGravShortcut = new ImageButton(this);
 
-			// ImageButton gravShortcut = new ImageButton(this);
-			//
-			// gravShortcut.setImageResource(R.drawable.module_icon_gravity);
-			//
-			// // Set the shortcut layout params
-			// LinearLayout.LayoutParams layoutParams = new
-			// LinearLayout.LayoutParams(
-			// LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
-			//
-			// // Make it fit in the sliding menu
-			// // gravShortcut.setScaleType(ScaleType.FIT_XY);
-			// gravShortcut.setScaleType(ScaleType.CENTER_INSIDE);
-			//
-			// navBar.addView(gravShortcut, layoutParams);
+			univGravShortcut.setImageResource(R.drawable.module_icon_one);
+
+			// Set the shortcut layout params
+			LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+					LayoutParams.MATCH_PARENT, navBar.findViewById(
+							R.id.Module_Button).getHeight());
+
+			// Make it fit in the sliding menu
+			// gravShortcut.setScaleType(ScaleType.FIT_XY);
+			univGravShortcut.setScaleType(ScaleType.CENTER_INSIDE);
+
+			navBar.addView(univGravShortcut, layoutParams);
 		}
 
 		fragmentTransaction.addToBackStack(null);
@@ -266,28 +310,33 @@ public class MainModuleSelector extends Activity {
 
 			Fragment opticsFrag = new Optics_Fragment();
 
-			fragmentTransaction.replace(R.id.moduleSelectorFragment_Landscape,
+			fragmentTransaction.remove(moduleSelector);
+
+			fragmentTransaction.add(R.id.moduleSelectorFragment_Landscape,
 					opticsFrag, "OPTICS_FRAG_LAND");
 
-			Fragment moduleFrag = fragmentManager
-					.findFragmentByTag("MODULE_FRAG_LAND");
+			// fragmentTransaction.replace(R.id.moduleSelectorFragment_Landscape,
+			// opticsFrag, "OPTICS_FRAG_LAND");
+			//
+			// Fragment moduleFrag = fragmentManager
+			// .findFragmentByTag("MODULE_FRAG_LAND");
+			//
+			// fragmentTransaction.remove(moduleFrag);
 
-			fragmentTransaction.remove(moduleFrag);
+			ImageButton opticsShortcut = new ImageButton(this);
 
-			// ImageButton gravShortcut = new ImageButton(this);
-			//
-			// gravShortcut.setImageResource(R.drawable.module_icon_gravity);
-			//
-			// // Set the shortcut layout params
-			// LinearLayout.LayoutParams layoutParams = new
-			// LinearLayout.LayoutParams(
-			// LayoutParams.MATCH_PARENT, LayoutParams.WRAP_CONTENT);
-			//
-			// // Make it fit in the sliding menu
-			// // gravShortcut.setScaleType(ScaleType.FIT_XY);
-			// gravShortcut.setScaleType(ScaleType.CENTER_INSIDE);
-			//
-			// navBar.addView(gravShortcut, layoutParams);
+			opticsShortcut.setImageResource(R.drawable.optics);
+
+			// Set the shortcut layout params
+			LinearLayout.LayoutParams layoutParams = new LinearLayout.LayoutParams(
+					LayoutParams.MATCH_PARENT, navBar.findViewById(
+							R.id.Module_Button).getHeight());
+
+			// Make it fit in the sliding menu
+			// gravShortcut.setScaleType(ScaleType.FIT_XY);
+			opticsShortcut.setScaleType(ScaleType.CENTER_INSIDE);
+
+			navBar.addView(opticsShortcut, layoutParams);
 		}
 
 		fragmentTransaction.addToBackStack(null);
